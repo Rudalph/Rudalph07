78075a5c663e56dea467b3fb9e22153bc4da06ef
Rudalph <gonsalvesrudalph@gmail.com>
2024-08-04 20:35:07 +0530

    Navbar added

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Navbar from "@/Components/Navbar";
 
 export default function Home() {
   return (
-    <>Rudalph</>
+    <div>
+      <Navbar />
+    </div>
   );
 }
