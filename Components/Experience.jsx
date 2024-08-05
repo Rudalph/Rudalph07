b01b8ac1ee91d75192098c134c9c00e1389dc9ff
Rudalph <gonsalvesrudalph@gmail.com>
2024-08-05 21:52:03 +0530

    Experience development initiated

---
+++ Components/Experience.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='mt-10'>
+        <div className="flex items-center lg:px-14 px-5">
+                <div className="text-sm lg:text-2xl text-white font-bold ">EXPERIENCE</div>
+                <div className="flex-grow border-t border-white mr-4 ml-4"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Navbar from "@/Components/Navbar";
 import Hero from "@/Components/Hero";
 import Projects from "@/Components/Projects";
 import Technologies from "@/Components/Technologies";
+import Experience from "@/Components/Experience";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <Projects />
       <Technologies />
+      <Experience />
     </div>
   );
 }
